b5b664bd73871e352a68f21b65f6b6519a982416
MartinCesano <cesano305@gmail.com>
2024-12-07 08:53:22 -0300

    Se reorganizaron carpetas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,8 @@
 import { Routes } from '@angular/router';
 import { LayoutComponent } from './shared/layout/layout.component';
-import { HomeComponent } from './home/home.component';
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
+import { HomeComponent } from './components/pages/home/home.component';
+import { LoginComponent } from './components/forms/login/login.component';
+import { RegisterComponent } from './components/forms/register/register.component';
 
 export const routes: Routes = [
   {
